86a08e7f6cad56f6946b5e170a294465564daf96
DngDavide02 <dangelodavide02@gmail.com>
2025-08-01 12:14:03 +0200

    Create NotFoundException.java

---
+++ src/main/java/dangelodavide/U2_W3_D5_Friday/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package dangelodavide.U2_W3_D5_Friday.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
