e52d43db9f00294bcafa162963704ce191a32bbe
DngDavide02 <dangelodavide02@gmail.com>
2025-08-01 11:10:32 +0200

    Unauthorized Exception created

---
+++ src/main/java/dangelodavide/U2_W3_D5_Friday/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package dangelodavide.U2_W3_D5_Friday.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
